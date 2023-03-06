58b2c1ae2c0d63638e629300388fe3a2724e5615
jmjjmj994 <jonathan.mjos@outlook.com>
2023-03-06 11:57:22 +0100

    working

--- front-page/js/interactive.js
+++ front-page/js/interactive.js
@@ -24,3 +24,4 @@ hamburgerMenu.addEventListener('click', () => {
 });
 
 
+
--- front-page/js/movies.js
+++ front-page/js/movies.js
@@ -1,3 +1,9 @@
+
+const slide = document.querySelector(".slide");
+
+const sliderBg = document.createElement("div")
+
+const posterPath = "http://image.tmdb.org/t/p/w500/9Rj8l6gElLpRL7Kj17iZhrT5Zuw.jpg "
 const key = "2e9d1426df14f8f8926141cd1d738566"
 const url ="https://api.themoviedb.org/3/trending/all/day?api_key=2e9d1426df14f8f8926141cd1d738566"
 fetch(url)
@@ -5,5 +11,14 @@ fetch(url)
     return res.json()
 })
 .then((data)=> {
-    console.log(data)
+  console.log(data)
+  data.results.forEach(img => {
+    const {poster_path} = img;
+    
+    
+    
+
+  });
+    
+  
 })
